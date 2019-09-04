2db685472c63d5270beed7f1962159f5d61cf5b0
boubtane mohamed ali <m.ali.boubtane@gmail.com>
2019-09-04 16:11:01 +0200

    Morse-lib

--- App.js
+++ App.js
@@ -2,11 +2,25 @@ import React from 'react';
 import {  Text, View } from 'react-native';
 import Header from './Components/Header.js';
 import Body from './Components/Body.js';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 
 export default function App() {
   return (
     <View style = {{flex: 1}}>
         <Header/>
+        <Button
+            icon={
+                <Icon
+                    name="arrow-right"
+                    size={15}
+                    color="white"
+                />
+            }
+            title="Button with icon component"
+        />
         <Body/>
     </View>
   );
