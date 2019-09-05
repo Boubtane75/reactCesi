2f7ad4c46720b5ea52ab361711edb60a497106c6
boubtane mohamed ali <m.ali.boubtane@gmail.com>
2019-09-05 09:09:24 +0200

    button audio and read

--- Style/BodyS.js
+++ Style/BodyS.js
@@ -3,6 +3,6 @@ import {StyleSheet} from 'react-native';
 export const BodyStyle= StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'lightblue'
+        backgroundColor: 'white'
     },
 });
